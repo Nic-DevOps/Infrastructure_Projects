e924ccca2f83ec2fcb7eced3b61d46eaace23f80
Nick <218025888+Nic-DevOps@users.noreply.github.com>
2025-07-02 20:56:01 +1200

    Resolve README.md conflict between main and fix-script

--- Project_2_Vagrant_HTTP_Server/bootstrap.sh
+++ Project_2_Vagrant_HTTP_Server/bootstrap.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+set -e
+
+
 # Update the package list to make sure we get the latest available packages
+echo "[+] Updating package list..."
 sudo apt-get update -y
 
+# Install Apache
+echo "[+] Installing Apache..."
+sudo apt-get install apache2 -y
+
 # Enable the Apache2 service to start on boot
+echo "[+] Enabling and starting Apache service..."
 sudo systemctl enable apache2
 
 # Start the Apache2 service immediately
@@ -12,4 +21,5 @@ sudo systemctl start apache2
 # Create a basic test HTML page to verify that the server is working
 # This command replaces the default index.html file
 # tee reads from standard input and writes to the given file.
+echo "[+] Writing index page..."
 echo "<h1>Hello from Apache via Vagrant!</h1>" | sudo tee /var/www/html/index.html
